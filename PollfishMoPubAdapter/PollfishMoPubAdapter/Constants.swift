843d48dbc535ed4799118490f6add12043b0e99b
Fotis Mitropoulos <fotismitropoulos@gmail.com>
2021-07-26 14:18:52 +0300

    Pollfish iOS MoPub Adapter v6.2.0.0

--- PollfishMoPubAdapter/PollfishMoPubAdapter/Constants.swift
+++ PollfishMoPubAdapter/PollfishMoPubAdapter/Constants.swift
@@ -9,9 +9,9 @@ import Foundation
 
 struct Constants {
     
-    static let version = "6.1.0.0"
+    static let version = "6.2.0.0"
     static let netwrokName = "pollfish"
-    static let pollfishVersion = "6.1.0"
+    static let pollfishVersion = "6.2.0"
     
     struct ExtraKey {
         static let apiKey = "api_key"
